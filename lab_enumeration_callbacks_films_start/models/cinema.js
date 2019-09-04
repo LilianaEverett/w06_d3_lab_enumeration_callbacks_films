9379d547005ff55b8ff824d601bef1b504a1025f
Liliana Everett <everettliliana@gmail.com>
2019-09-04 15:29:05 +0100

    map refactored

--- lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -2,11 +2,9 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
 Cinema.prototype.getAllFilmTitles = function(){
-  const filmTitles = this.films.map (function(film) {
-    return film.title
-  })
-  return filmTitles
+  return this.films.map (film => film.title)
 }
 
 Cinema.prototype.findByTitle = function(title) {
